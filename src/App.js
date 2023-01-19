adc32e0798f46867892108eb2dcce63b51de98ec
cheito70 <49110712+cheito70@users.noreply.github.com>
2023-01-19 13:16:20 -0600

    commit title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+//import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Really Learn React
-        </a>
-      </header>
+       <CitySearch />
+       <EventList />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { mockData } from '../mock-data';
+import Event from '../Event';
+
+describe( '<Event /> component', () => {
+    let EventWrapper;
+    const event = mockData[0];
+    beforeAll(() => {
+        EventWrapper = shallow(<Event event={event}/> );
+    });
+
+    test('renders the component', () => {
+        expect(EventWrapper).toBeDefined();
+    });
+
+    test('renders the summary in an h2 tag', () => {
+        const summary = EventWrapper.find('h2.summary');
+
+        expect(summary).toHaveLength(1);
+        expect(summary.text()).toBe(event.summary);
+    });
+
+    test('renders the start details in a paragraph', () => {
+        const eventStart = EventWrapper.find('p.event-start');
+
+        expect(eventStart).toHaveLength(1);
+        expect(eventStart.text()).toBe(new Date(event.start.dateTime).toString());
+    });
+
+    test('renders the location details in a paragraph', () => {
+        const eventLocation = EventWrapper.find('p.event-location');
+    
+        expect(eventLocation).toHaveLength(1);
+        expect(eventLocation.text()).toBe(`@${event.summary} | ${event.location}`);
+      });
+    
+    test('renders the "show details" button when collapsed', () => {
+        const detailsButton = EventWrapper.find('button.details-btn');
+
+        expect(detailsButton).toHaveLength(1);
+        expect(detailsButton.text()).toBe('show details');
+    });
+
+})
